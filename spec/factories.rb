07750d2c3e77d72944c0d1c34b181e0913c79ac3
Brent Haas <brenthaas@gmail.com>
2015-05-18 13:46:37 -0700

    Fixed factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    first_name "John"
-    last_name  "Doe"
-    email "example@example.com"
+    sequence(:first_name){|n| "First#{n}" }
+    sequence(:last_name){|n| "Last#{n}" }
+    sequence(:email){|n| "user#{n}@factory.com" }
     password "example123"
     password_confirmation "example123"
   end
