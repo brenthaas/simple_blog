55c470d9629adfb338e8f5c930647be643939c4b
Brent Haas <brenthaas@gmail.com>
2015-05-19 15:19:06 -0700

    cleaned up new post submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    post = Post.create(post_params)
+    if post.persisted?
+      redirect_to post
+    else
+      flash[:error] = "Unable to save! #{post.errors}"
+      redirect_to :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :posted_at)
+  end
 end
